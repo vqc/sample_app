def4c2a6a79ab22740f63f7882c971802c44d7eb
Vincent Chuang <vince@cvq.cc>
2013-09-12 06:33:35 +0000

    :q

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -32,7 +32,7 @@ describe "Static pages" do
   describe "Contact Page" do
     before {visit contact_path}
     
-    it {should have_content('Contact')}
+    it {should have_selector('h1', text: 'Contact')}
     it {should have_title("#{base_title} | Contact")}
   end
   
