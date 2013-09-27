a54938f02ba9acd3a0eac2cd5ea54f39637b6c80
Vincent Chuang <vince@cvq.cc>
2013-09-27 20:01:13 +0000

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name	"Vincent Chuang"
+    email	"vince@cvq.cc"
+    password	"foobar"
+    password_confirmation "foobar"
+  end
+end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,12 +1,55 @@
 require 'spec_helper'
 
 describe "User Pages" do
+
   subject{page}
   let(:base_title) {"Ruby on Rails Tutorial Sample App"}
-  describe "Sign Up Page" do
+
+  describe "profile page" do
+
+    let(:user) {FactoryGirl.create(:user) } #This mcode makes a user variable
+    #The create method is found in spec/factories.rb. It creates a ":user"
+    #with name, email, password, and password_confirmation attributes
+    before {visit user_path(user)}
+
+    it { should have_content(user.name)}
+    it { should have_title(user.name)}
+
+  end
+
+  describe "signup page" do
+
     before {visit signup_path}
     
     it { should have_content('Sign Up')}
     it { should have_title("#{base_title} | Sign Up")}
+    
   end
+ 
+  describe "signup" do
+
+    before { visit signup_path }
+
+    let(:submit) { "Create my account" }
+
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end
+
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
+  end
+
 end
